71d64a5d5ac376bbe444d7eb5d3f4470e5c38c0b
sanjan <sanjanpoojary36@gmail.com>
2025-11-28 11:12:14 +0530

    commit to git

--- server.js
+++ server.js
@@ -4,13 +4,19 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const Task = require('./models/Task');
 const app = express();
+const path = require("path");
+
 app.use(cors());
 app.use(express.json());
+
+// Serve frontend
+app.use(express.static(path.join(__dirname, "frontend")));
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("DB Connected"))
   .catch((err) => console.error(err));
 
+// API routes
 app.get('/tasks', async (req, res) => {
   const tasks = await Task.find();
   res.json(tasks);
@@ -25,6 +31,11 @@ app.post('/tasks', async (req, res) => {
 app.delete('/tasks/:id', async (req, res) => {
   await Task.findByIdAndDelete(req.params.id);
   res.json({ message: "Deleted" });
+});
+
+// Catch-all – FIXED
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "index.html"));
 });
 
 app.listen(5000, () => console.log("Server running on port 5000"));
